031003429bb7c3416e5729890b4ebc67985fe3e0
James Demiraiakian <james.armen@gmail.com>
2022-02-15 15:17:59 -0800

    edit tiems is work

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+
 export default function Item({ item, onEdit, onDelete }) {
+  const [isEdit, setIsEdit] = useState(false);
+  let content;
+
+  if (!isEdit) {
+    content = (
+      <div>
+        <button type="button" onClick={() => setIsEdit(true)}>
+          Edit
+        </button>
+      </div>
+    );
+  } else {
+    content = (
+      <div>
+        <input
+          type="text"
+          value={item.text}
+          onChange={(e) => onEdit({ ...item, text: e.target.value })}
+        />
+        <button type="button" onClick={() => setIsEdit(false)}>
+          Save
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div>
       <span>{item.text}</span>
+      {content}
     </div>
   );
 }
