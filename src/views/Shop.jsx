c7cb51b65387cb43ccaee0b8766ed1bf01b09a16
James Demiraiakian <james.armen@gmail.com>
2022-02-14 16:38:54 -0800

    a couple files and initialItems array

---
+++ src/views/Shop.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+
+const initialItems = [
+  { id: 0, text: 'beef 🐄', done: false },
+  { id: 1, text: 'apple 🍏', done: false },
+  { id: 2, text: 'lettuce 🍃', done: false },
+];
+
+export default function Shop() {}
