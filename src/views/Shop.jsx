4ffb740a10a36cda9c372b25a45697dbef49d4eb
James Demiraiakian <james.armen@gmail.com>
2022-02-14 17:13:24 -0800

    wrote the reducer function, useReducer hook, and handler functions

--- src/views/Shop.jsx
+++ src/views/Shop.jsx
@@ -1,4 +1,4 @@
-import react from 'react';
+import react, { useReducer } from 'react';
 
 const initialItems = [
   { id: 0, text: 'beef 🐄', done: false },
@@ -6,4 +6,40 @@ const initialItems = [
   { id: 2, text: 'lettuce 🍃', done: false },
 ];
 
-export default function Shop() {}
+function itemReducer(items, action) {
+  switch (action.type) {
+    case 'add': {
+      return [...items, { id: action.id, text: action.text, done: false }];
+    }
+    case 'edit': {
+      return items.map((item) => {
+        if (item.id === action.entry.id) {
+          return action.entry;
+        }
+        return item;
+      });
+    }
+    case 'delete':
+      return items.filter((item) => item.id !== action.item);
+    default:
+      throw new Error(
+        'There is an error that is causing the switch to hit the Default Case. Please debug'
+      );
+  }
+}
+
+export default function Shop() {
+  const [items, dispatch] = useReducer(itemReducer, initialItems);
+
+  const handleAdd = (text) => {
+    dispatch({ type: 'add', id: items.length + 1, text });
+  };
+
+  const handleEdit = (entry) => {
+    dispatch({ type: 'edit', entry });
+  };
+
+  const handleDelete = (entryId) => {
+    dispatch({ type: 'delete', entryId });
+  };
+}
